a97cd318fb0ac3e96db608b3189e3d16472e1653
Leonardo A R De Oliveira <leonardo.oliveira@negocioanimal.com.br>
2022-09-08 08:32:29 -0300

    feat(init): config react navigation on application and setup plugins

---
+++ src/routes/index.routes.tsx
@@ -0,0 +1,13 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "../pages/Home";
+
+const Stack = createNativeStackNavigator();
+
+const Router = () => <NavigationContainer>
+    <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} />
+    </Stack.Navigator>
+</NavigationContainer>
+
+export default Router;
